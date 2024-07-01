534614a0bc164fa34db16fde20d377e31179040d
Roman Volodin <roman.volodin@gmail.com>
2024-07-01 22:02:42 +0300

    Get info (fps, etc) from selected strip

--- ffautocut.py
+++ ffautocut.py
@@ -1,6 +1,16 @@
 import json
 import subprocess
 
+import bpy
+
+
+def filter_selected_strips(context, types=('MOVIE',)):
+    selected_strips = [
+        strip for strip in context.selected_sequences if strip.type in types
+    ]
+    if selected_strips:
+        return selected_strips
+
 
 def detect_cuts_with_ffprobe(ffprobe, filepath, time_start, time_end, threshold=0.3):
     output = subprocess.check_output(
@@ -14,14 +24,20 @@ def detect_cuts_with_ffprobe(ffprobe, filepath, time_start, time_end, threshold=
 
 
 if __name__ == "__main__":
-    out = detect_cuts_with_ffprobe(
-        ffprobe="ffprobe",
-        filepath="test_clip.mov",
-        time_start=0,
-        time_end=49,
-        threshold=0.1
-    )
+    selected_strips = filter_selected_strips(bpy.context)
 
-    for cut in out['frames']:
-        time = float(cut['pkt_dts_time'])
-        print(time, time*24, round(time*24))
+    for strip in selected_strips:
+        strip_timeline_offset = strip.frame_final_start
+
+        out = detect_cuts_with_ffprobe(
+            ffprobe="ffprobe",
+            filepath=strip.filepath,
+            time_start=0,
+            time_end=49,
+            threshold=0.1
+        )
+
+        for cut in out['frames']:
+            time_in_seconds = float(cut['pkt_dts_time'])
+            time_in_frames = round(time_in_seconds * strip.fps) + strip_timeline_offset
+            print(time_in_seconds, time_in_frames)
